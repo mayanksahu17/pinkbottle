91667e499e25a81ff6421e9aac8844da3c018a70
Varghab Shib <shibvarghab@gmail.com>
2024-04-28 18:27:50 +0530

    test

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -4,7 +4,6 @@ import Stripe from 'stripe';
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: '2023-10-16',
 });
-
 export async function POST(req: NextRequest) {
   if (req.method === 'POST') {
     try {
