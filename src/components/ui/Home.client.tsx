0ec245b6423bef8e0eb2bf82d4da3787ecb70f85
Anshul Jain <anshulj07@gmail.com>
2024-07-09 20:14:38 -0400

    Blackrock image

--- src/components/ui/Home.client.tsx
+++ src/components/ui/Home.client.tsx
@@ -427,7 +427,7 @@ const HomePage = () => {
           <div className="mt-10 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-8 items-center">
             <div className="flex justify-center items-center h-16 w-full">
               <img
-                src="Blackrock.svg"
+                src="blackrock.svg"
                 alt="Blackrock"
                 className="h-full object-contain"
               />
