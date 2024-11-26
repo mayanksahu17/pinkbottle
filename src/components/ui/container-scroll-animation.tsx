bba59964b02f1ef6607e7ed7e92a8837ba9e890f
ayush-maddhesiya <ayushkumarmaddhesiya.com>
2024-11-26 22:05:22 +0530

    update container height and adjust margin for improved layout

--- src/components/ui/container-scroll-animation.tsx
+++ src/components/ui/container-scroll-animation.tsx
@@ -36,11 +36,11 @@ export const ContainerScroll = ({
 
   return (
     <div
-      className="h-[40rem] md:h-[50rem] flex items-center justify-center relative mb-10 bg-slate-200 overflow-hidden"
+      className="h-[60rem] md:h-[60rem] flex items-center bg-slate-200 justify-center relative"
       ref={containerRef}
     >
       <div
-        className="py-3 md:py-15 w-full relative"
+        className="w-full relative"
         style={{
           perspective: "1000px",
         }}
@@ -85,7 +85,7 @@ export const Card = ({
         boxShadow:
           "0 0 #0000004d, 0 9px 20px #0000004a, 0 37px 37px #00000042, 0 84px 50px #00000026, 0 149px 60px #0000000a, 0 233px 65px #00000003",
       }}
-      className="max-w-5xl -mt-6 mx-auto h-[30rem] md:h-[40rem] w-full border-4 border-[#6C6C6C] p-2 md:p-6 rounded-[30px] shadow-2xl"
+      className="max-w-5xl -mt-12 mx-auto h-[30rem] md:h-[40rem] w-full border-4 border-[#6C6C6C] p-2 md:p-6 rounded-[30px] shadow-2xl"
     >
       <div className=" h-full w-full  overflow-hidden rounded-2xl bg-gray-100 dark:bg-zinc-900 md:rounded-2xl md:p-4 ">
         {children}
