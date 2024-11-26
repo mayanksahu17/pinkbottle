5ca108afa2061e09163a74bd4e42f956cef187bc
ayush-maddhesiya <ayushkumarmaddhesiya.com>
2024-11-26 21:40:46 +0530

    button effect added

--- src/components/Home/Message.tsx
+++ src/components/Home/Message.tsx
@@ -93,8 +93,8 @@ const Message = () => {
           </div>
         </div>
         <div className="flex flex-col md:flex-row gap-4 mt-10 items-center justify-center">
-          <Link href="/onboarding">
-            <button className="relative flex items-center justify-center gap-2 px-6 py-2 text-sm md:text-lg font-medium text-black bg-white rounded-full hover:bg-gray-100 transition-all shadow-lg border border-gray-300 w-full md:w-auto">
+        <Link href="/onboarding">
+            <button className="relative flex items-center justify-center gap-2 px-6 py-2 text-sm md:text-lg font-medium text-black bg-white rounded-full hover:bg-green-50 transition-all shadow-lg border border-gray-300 w-full md:w-auto animate-shimmer bg-[linear-gradient(110deg,#d2f8e3,45%,#b6e7d9,55%,#d2f8e3)] bg-[length:200%_100%] focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-offset-2 focus:ring-offset-slate-50">
               Schedule 3 days free trial
               <svg
                 xmlns="http://www.w3.org/2000/svg"
