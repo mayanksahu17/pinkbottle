22545bbb320fee974433c00f3a275cda479e9adf
ayush-maddhesiya <ayushkumarmaddhesiya.com>
2024-11-26 07:06:48 +0530

    less padding

--- src/components/Home/Features.tsx
+++ src/components/Home/Features.tsx
@@ -9,7 +9,7 @@ export function Features() {
   ));
 
   return (
-    <div className="w-full h-full py-20">
+    <div className="w-full h-full py-15 mt-5 mb-5">
       <h2 className="max-w-7xl pl-4 mx-auto text-xl md:text-5xl font-bold text-neutral-800 dark:text-neutral-200 font-sans">
         Get to know your iSad.
       </h2>
